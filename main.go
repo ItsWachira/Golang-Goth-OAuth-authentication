51fdf5566eed5f81a92a462d9f56cbf208df089d
ItsWachira <brianwachira79@gmail.com>
2024-05-11 16:11:24 +0300

    feat: added google social login using goth's oauth provider

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+
+
+func main() {
+	r := gin.Default()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env file failed to load!")
+	}
+
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	clientCallbackURL := os.Getenv("CLIENT_CALLBACK_URL")
+
+	if clientID == "" || clientSecret == "" || clientCallbackURL == "" {
+		log.Fatal("Environment variables (CLIENT_ID, CLIENT_SECRET, CLIENT_CALLBACK_URL) are required")
+	}
+
+	goth.UseProviders(
+		google.New(clientID, clientSecret, clientCallbackURL))
+
+	r.LoadHTMLGlob("templates/*")
+
+	
+	r.GET("/", home)
+	r.GET("/auth/:provider", signInWithProvider)
+	r.GET("/auth/:provider/callback", callbackHandler)
+	r.GET("/success", Success)
+
+	r.Run(":5000")
+}
+
+func home(c *gin.Context) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(c.Writer, gin.H{})
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+}
+
+func signInWithProvider(c *gin.Context) {
+	provider := c.Param("provider")
+	q := c.Request.URL.Query()
+	q.Add("provider", provider)
+	c.Request.URL.RawQuery = q.Encode()
+
+	gothic.BeginAuthHandler(c.Writer, c.Request)
+}
+
+func callbackHandler(c *gin.Context) {
+	provider := c.Param("provider")
+	q := c.Request.URL.Query()
+	q.Add("provider", provider)
+	c.Request.URL.RawQuery = q.Encode()
+
+	_, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	
+
+	c.Redirect(http.StatusTemporaryRedirect, "/success")
+}
+
+func Success(c *gin.Context) {
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(fmt.Sprintf(`
+      <div style="
+          background-color: #fff;
+          padding: 40px;
+          border-radius: 8px;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+          text-align: center;
+      ">
+          <h1 style="
+              color: #333;
+              margin-bottom: 20px;
+          ">You have Successfully signed in!</h1>
+          
+          </div>
+      </div>
+  `)))
+}
